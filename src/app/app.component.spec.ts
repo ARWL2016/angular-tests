dd978113e5bcbcbc77a0943e7db173c2ef925b91
Alistair Willis <alistairrwillis@gmail.com>
2017-10-23 22:58:02 +0900

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,13 +5,14 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 describe('AppComponent', function () {
-  let de: DebugElement;
+  let debug: DebugElement;
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let h1: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ]  // declare the test component
     })
     .compileComponents();
   }));
@@ -19,15 +20,26 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+    debug = fixture.debugElement.query(By.css('h1'));
+    h1 = debug.nativeElement;
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
-    const h1 = de.nativeElement;
+
     expect(h1.innerText).toMatch(/angular/i,
       '<h1> should say something about "Angular"');
+    expect(h1.innerText).toContain(comp.name);
+    expect(h1.innerText).toEqual('Hello Angular');
   });
+
+  it('should show a different h1', () => {
+    comp.name = 'Testing';
+    fixture.detectChanges();
+    expect(h1.innerText).toContain(comp.name);
+  });
+
+
 });
